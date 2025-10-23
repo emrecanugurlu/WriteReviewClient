5fe4a065da1cae408993f106f6ad30f6c44226cb
Emre Can UĞURLU <emrecnugurlu@gmail.com>
2025-10-23 16:50:37 +0300

    jwt, Authentication, Authorization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,15 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {apiBaseInterceptor} from './interceptors/base-interceptor';
+import {authInterceptor} from './interceptors/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([apiBaseInterceptor,authInterceptor]))
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {Login} from './components/login/login';
+import {Home} from './components/home/home';
+import {MyArticles} from './components/my-articles/my-articles';
+import {NewArticle} from './components/new-article/new-article';
+import {UpdateArticle} from './components/update-article/update-article';
+import {Profile} from './components/profile/profile';
+import {StaffInbox} from './components/staff-inbox/staff-inbox';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'new-article', component: NewArticle},
+  {path: 'login', component: Login},
+  {path: 'my-articles', component: MyArticles},
+  {path: 'update-article/:id', component: UpdateArticle},
+  {path: 'profile', component: Profile},
+  {path: 'home', component: Home},
+  { path: 'staff/inbox', component: StaffInbox },
+];
